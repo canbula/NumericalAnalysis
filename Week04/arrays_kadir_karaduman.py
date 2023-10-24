3d84eeebe4e50f1a40faeddcc0c0109d342b8085
Kadir Karaduman <40742246+kadirkaraduman@users.noreply.github.com>
2023-10-24 18:25:06 +0300

    Create arrays_kadir_karaduman.py

---
+++ Week04/arrays_kadir_karaduman.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def replace_center_with_minus_one(d, n, m):
+
+    if(m > n or d<= 0 or n < 0 or m < 0):
+        raise ValueError("Value Error has been ocurred")
+
+
+    end = (10**d) - 1
+
+    array = np.random.randint(0, end, size=(n, n))
+
+    start_index = (n - m) // 2
+
+    central_part = array[start_index:start_index + m, start_index:start_index + m]
+
+    for i in range(start_index, start_index+m):
+        for j in range(start_index, start_index+m):
+            array[i, j] = -1
+
+
+    return array
