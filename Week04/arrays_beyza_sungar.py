76464e90ebb40f5652d2d446c86722fc6d51e4ca
Beyza Sungar <107926225+bsungar@users.noreply.github.com>
2023-10-25 23:50:43 +0300

    arrays_beyza_sungar.py

--- Week04/arrays_beyza_sungar.py
+++ Week04/arrays_beyza_sungar.py
@@ -1,32 +1,27 @@
 import numpy as np
 
-try:
-    n = int(input("Enter a dimension number (n): "))
+def replace_center_with_minus_one(n, d, m):
     if n <= 0:
         raise ValueError("n must be a positive integer")
-
-    num_digits = int(input("Enter the number of digits for random integers: "))
-    if num_digits <= 0:
+    if d <= 0:
         raise ValueError("num_digits must be a positive integer")
-
-    m = int(input("Enter the size of the central array (m): "))
     if m <= 0:
         raise ValueError("m must be a positive integer")
     if m > n:
         raise ValueError("m cannot be greater than n")
+    
+    arr1 = np.random.randint(10 ** (d - 1), 10 ** d, size=(n, n))
+    arr1[1:n-1, 1:n-1] = -1
+    return arr1
+
+try:
+    n = int(input("Enter a dimension number (n): "))
+    d = int(input("Enter the number of digits for random integers: "))
+    m = int(input("Enter the size of the central array (m): "))
+    
+    result_array = replace_center_with_minus_one(n, d, m)
+    
+    print("Modified Array:")
+    print(result_array)
 except ValueError as e:
     print(f"Error: {e}")
-    exit()
-
-
-arr1 = np.random.randint(10 ** (num_digits - 1), 10 ** num_digits, size=(n, n))
-
-#print(arr1)
-
-def change_edge_elements(arr):
-    n, m = arr.shape
-    arr[1:n-1, 1:m-1] = -1
-    return arr
-
-result_array = change_edge_elements(arr1)
-print(result_array)
