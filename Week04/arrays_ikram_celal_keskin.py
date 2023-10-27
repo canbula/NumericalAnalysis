e7acc6a03c86f27b26d51aa39921e8515830398b
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2023-10-27 15:25:03 +0300

    Update arrays_ikram_celal_keskin
    -I have corrected my mathematical error
    -I replaced the faulty argument
    -I didn't forget to put it to the test this time :D

--- Week04/arrays_ikram_celal_keskin.py
+++ Week04/arrays_ikram_celal_keskin.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def replace_center_with_minus_one(n, d, m):
-    if (m < 0 or n < 0 or m > n or d <= 0) :
-        raise ValueError("m should be less than or equal to n")
+def replace_center_with_minus_one(d, n, m):
+    if ( d <= 0 or n < 0 or m < 0 or m > n ):
+        raise ValueError("Erroneous data encountered")
 
-    rarr = np.random.randint(10 ** (d - 1), 10 ** d, (n, n))
+    rarr = np.random.randint(0, 10 ** (d - 1), size=(n, n))
 
     start_idx = (n - m) // 2
     end_idx = start_idx + m
