4225c8fe13a77f189dd2fbd70a4686d52cc76dd3
Emre-Bi <126341187+Emre-Bi@users.noreply.github.com>
2023-10-27 16:28:26 +0300

    arrays_emre_bicer.py

---
+++ Week04/arrays_emre_bicer.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def replace_center_with_minus_one(d, n, m):
+    if(d <= 0):
+        raise ValueError("d can not be less than or equal to 0")
+    elif(m > n):
+        raise ValueError("n can not be less than m")
+    elif(n < 0):
+        raise ValueError("n can not be less than 0")
+    elif(m < 0):
+        raise ValueError("m can not be less than 0")
+
+    highest_rand_num = int('9' * d)
+
+    np_array = np.random.randint(0, (highest_rand_num+1), size=(n, n))
+
+    start_index = int((n - m) / 2)
+    end_index = int(start_index + m)
+
+    np_array[start_index:end_index, start_index:end_index] = -1
+
+    return np_array
