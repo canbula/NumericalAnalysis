31ac1b630d481af5aba0c296c0799289c841d3af
yaseminkoc <yaseminkocc37@gmail.com>
2023-10-29 18:43:08 +0300

    variable types fixed

--- Week04/arrays_yasemin_koc.py
+++ Week04/arrays_yasemin_koc.py
@@ -5,7 +5,7 @@ import numpy as np
 # d: number of digits
 # m: m x m matrix in the center
 
-def replace_center_with_minus_one(n, d, m):
+def replace_center_with_minus_one(d: int, n: int, m: int):
     if n < 0 or m > n or d <= 0 or m < 0 or m > n:
         raise ValueError("Invalid parameter(s)")
 
@@ -13,8 +13,8 @@ def replace_center_with_minus_one(n, d, m):
     generated_random_matrix = np.random.randint(10 ** (d - 1), 10 ** d - 1, (n, n))
     # print(generated_random_matrix)
     y, x, *_ = generated_random_matrix.shape
-    start_x = x // 2 - (m // 2)
-    start_y = y // 2 - (m // 2)
+    start_x = (x - m) // 2
+    start_y = (y - m) // 2
     # minus_one_matrix = np.full((m, m), -1)
     generated_random_matrix[start_y:start_y + m, start_x:start_x + m, ...] = -1
     return generated_random_matrix
