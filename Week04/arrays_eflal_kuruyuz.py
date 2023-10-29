20d7bc6172b419ccef7667b44bf1ea862ec9bfd8
Eflal Kuruyüz <116119162+Eflalkryz@users.noreply.github.com>
2023-10-29 16:10:04 +0300

    Update arrays_eflal_kuruyuz.py

--- Week04/arrays_eflal_kuruyuz.py
+++ Week04/arrays_eflal_kuruyuz.py
@@ -1,25 +1,26 @@
 import numpy as np
 
 def replace_center_with_minus_one(n, d, m):
+    
+    
+    if m > n or d <= 0 or n < 0 or m < 0:
+        raise ValueError("Value Error has been ocurred")
+       
+    
+    max_value = 10**d - 1
+    random_array = np.random.randint(0, max_value+1, size=(n, n))
     max_value = 10**d - 1
     random_array = np.random.randint(0, max_value+1, size=(n, n))
     
-    if m > n or d <= 0 or n < 0 or m < 0:
-        print("please follow the value rules and try again.")
-       
-    else:
-        max_value = 10**d - 1
-        random_array = np.random.randint(0, max_value+1, size=(n, n))
+    center_start = (n - m) // 2
+    center_end = center_start + m
     
-        center_start = (n - m) // 2
-        center_end = center_start + m
+    random_array[center_start:center_end, center_start:center_end] = -1
+    print(random_array)
     
-        random_array[center_start:center_end, center_start:center_end] = -1
-        print(random_array)
-    
-        return random_array
+    return random_array
 
-replace_center_with_minus_one(5, 9, 3)
+
 
 
 
