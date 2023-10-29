2dd9061ea280c2b950686ccb14551af9646136dd
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-10-30 00:44:19 +0300

    arrays_bedirhan_elcin.py

--- Week04/arrays_bedirhan_elcin.py
+++ Week04/arrays_bedirhan_elcin.py
@@ -1,38 +1,20 @@
-
-#190316054 Bedirhan ELÇİN
-
-import random
+#Bedirhan elçin 190316054
 import numpy as np
 
+def replace_center_with_minus_one(n, d, m):
+    if m > n:
+        raise ValueError("m can't be greater than n")
+    elif d != 2:
+        raise ValueError("d must be 2 for two-digit numbers")
+    elif m < 0 or n < 0:
+        raise ValueError("m or n can't be lesser than 0")
 
-def create_random_matrix(n, m):
-    # m x m boyutunda, n adet rastgele 2 basamaklı sayı içeren bir matris oluşturma işlemi.
-    matrix = np.random.randint(10, 100, (m, m))
-    return matrix
-
-
-def replace_center(matrix):
-    m = len(matrix)
+    matrix = np.random.randint(10, 100, (n, n))
 
     if m >= 3:
-        # Merkezdeki 3x3 bölgeyi bulma işlemi
-        center_start = (m - 3) // 2
+
+        center_start = (n - 3) // 2
         center_end = center_start + 3
-
-        # Merkezdeki 3x3 bölgeyi -1 ile değiştirme işlemi
         matrix[center_start:center_end, center_start:center_end] = -1
-    else:
-        print("Matris boyutu en az 3x3 olmalıdır.")
 
-
-n = 5  #5 adet 2 basamaklı rastgele sayı içeren matris oluşturdum
-m = 5  #5x5 boyutunda bir matris oluşturdum
-
-matrix = create_random_matrix(n, m)
-print("Oluşturulan Matris:")
-print(matrix)
-
-replace_center(matrix)
-print("Merkezde -1 ile değiştirilmiş Matris:")
-print(matrix)
-
+    return matrix
