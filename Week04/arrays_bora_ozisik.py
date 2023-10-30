53cfa9c4966b40c8d817892f83dd01c34899a924
Bora Özışık <ozisikbora@gmail.com>
2023-10-30 11:33:25 +0300

    Update arrays_bora_ozisik.py

--- Week04/arrays_bora_ozisik.py
+++ Week04/arrays_bora_ozisik.py
@@ -1,16 +1,20 @@
 import numpy as np
 
-def replace_center_with_minus_one(d,n,m):
+def replace_center_with_minus_one(d, n, m):
 
-    if(m > n | d <= 0 | n < 0 | m < 0):
+    if(m > n or d<= 0 or n < 0 or m < 0):
         raise ValueError("Value Error: Invalid value used.Check parameters.")
 
 
-    numpy_array = np.random.randint(0, (10 ** d) - 1, size=(n, n))
+    end = (10**d) - 1
+
+    numpy_array = np.random.randint(0, end, size=(n, n))
 
     start_index = (n - m) // 2
-    end_index = start_index + m
 
-    numpy_array[start_index:end_index, start_index:end_index] = -1
-    
+    for i in range(start_index, start_index+m):
+        for j in range(start_index, start_index+m):
+            numpy_array[i, j] = -1
+
+
     return numpy_array
