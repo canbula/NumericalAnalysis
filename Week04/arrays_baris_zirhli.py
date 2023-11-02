bc0f9670df5371b959c0afcaca93e2ff6f41e24d
BarisZirhli <61722105+BarisZirhli@users.noreply.github.com>
2023-11-02 18:35:25 +0300

    arrays_baris_zirhli.py

---
+++ Week04/arrays_baris_zirhli.py
@@ -0,0 +1,11 @@
+def replace_center_with_minus_one(n: int, m: int, d: int) -> np.ndarray:
+
+    if ( d <= 0 or n < 0 or m < 0 or m > n ):
+        raise ValueError("Invalid parameter detection")
+
+    base_array = np.random.randint(0, 10**d-1, size=(n, n))
+    # We assume dimensions are always odd numbers
+    subArray_start = (n - m) // 2
+    subArray_end = subArray_start + m
+    base_array[subArray_start:subArray_end, subArray_start:subArray_end] = -1
+    return base_array
