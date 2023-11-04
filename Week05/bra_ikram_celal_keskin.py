32cda02e671e227ceafb42c7cb905a88bc43f8e8
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2023-11-04 18:56:45 +0300

    update bra_ikram_celal_keskin

--- Week05/bra_ikram_celal_keskin.py
+++ Week05/bra_ikram_celal_keskin.py
@@ -1,41 +1,94 @@
+import math
 from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
-def convert_to_binary(number):
-    try:
-        number = float(number)
-    except ValueError:
-        return None
 
-    if number.is_integer():
-        return bin(int(number))[2:]
-    else:
-        integer_part = bin(int(number))[2:]
-        fractional_part = ""
-        fractional = number - int(number)
+class BinaryRepresentation:
+    def __init__(self, number):
+        if isinstance(number, bool):
+            raise TypeError("Invalid input type, expected int or float")
+        if not isinstance(number, (int, float)):
+            raise TypeError("Invalid input type, expected int, float or bool")
+        self.number = number
 
-        while fractional > 0:
-            fractional *= 2
-            bit = int(fractional)
-            fractional_part += str(bit)
-            fractional -= bit
+    def get_number(self):
+       try:
+           num = float(self.number)
+           #calculate here
+           number_as_string = calculate_binary(num)
+           if str(num).endswith(".0"):
+               number_as_string = str(num).rstrip('0')
+           return number_as_string
+       except (ValueError, TypeError):
+           raise TypeError("Invalid input type, expected a valid number")
 
-        return f"{integer_part}.{fractional_part}"
+
+def calculate_binary(number):
+    part_of_integer = integer_part(number)
+    part_of_decimal = decimal_part(number)
+    return f"{part_of_integer}.{part_of_decimal}"
+
+def integer_part(number):
+    if number == 0:
+        return "0"
+
+    result = ""
+    number_of_integer = int(number)
+
+    while number_of_integer > 0:
+        remainder = number_of_integer % 2
+        result = str(remainder) + result
+        number_of_integer = number_of_integer // 2
+
+    return result
+
+
+def decimal_part(number):
+   decimal_part = math.modf(number)
+   if decimal_part == 0:
+       return ''
+
+   binary = ''
+   while decimal_part > 0:
+       decimal_part *= 2
+       bit = int(decimal_part)
+       binary += str(bit)
+       decimal_part -= bit
+   return binary[:10]
+
+
 
 @app.route('/', methods=['GET'])
 def binary_representation_api():
-    number = request.args.get('number')
+   number = request.args.get('number')
 
-    if number is None:
-        return jsonify({"error": "Lütfen geçerli bir sayıyı içeren bir GET isteği gönderin."}), 400
+   if number is None:
+       return jsonify({"error": "Please send a GET request to /?number=<number>"}), 400
 
-    binary_representation = convert_to_binary(number)
+   try:
+       # Convert the string to a boolean or a number
+       if number.lower() == 'true':
+           number = True
+       elif number.lower() == 'false':
+           number = False
+       else:
+           number = float(number)
+   except ValueError:
+       return jsonify({"error": "Please send a GET request to /?number=<number> with a valid number"}), 400
 
-    if binary_representation is not None:
-        return jsonify({"binary_representation": binary_representation}), 200
-    else:
-        return jsonify({"error": "Geçerli bir sayı gönderilmedi."}), 400
+   binary_representation = BinaryRepresentation(number)
+   return jsonify({"binary_representation": f"{binary_representation.get_number()}"}), 200
 
-if __name__ == '__main__':
+
+@app.errorhandler(400)
+def bad_request(e):
+   if isinstance(e, TypeError):
+       return jsonify({"error": "Invalid input type, expected a valid number"}), 400
+   else:
+       return jsonify({"error": str(e)}), 400
+
+
+if __name__== '__main__':
     app.run(debug=True)
