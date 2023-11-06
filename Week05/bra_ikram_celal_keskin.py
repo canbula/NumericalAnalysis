2c9ec01fed9be716322a2339781010b4f8ee6c26
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2023-11-06 11:25:24 +0300

    Update by @icelal_kskn

--- Week05/bra_ikram_celal_keskin.py
+++ Week05/bra_ikram_celal_keskin.py
@@ -1,4 +1,3 @@
-import math
 from flask import Flask, request, jsonify
 
 
@@ -10,77 +9,78 @@ class BinaryRepresentation:
         if isinstance(number, bool):
             raise TypeError("Invalid input type, expected int or float")
         if not isinstance(number, (int, float)):
-            raise TypeError("Invalid input type, expected int, float or bool")
-        self.number = number
+            raise TypeError("Invalid input type, expected int, float, or bool")
+        try:
+            self.number = float(number)
+        except ValueError:
+            raise TypeError("Input cannot be converted to a float")
 
     def get_number(self):
-       try:
-           num = float(self.number)
-           #calculate here
-           number_as_string = calculate_binary(num)
-           if str(num).endswith(".0"):
-               number_as_string = str(num).rstrip('0')
-           return number_as_string
-       except (ValueError, TypeError):
-           raise TypeError("Invalid input type, expected a valid number")
+        integer_part_str = self.integer_part(int(self.number))
+        decimal_part_str = self.decimal_part(self.number - int(self.number))
 
+        return f"{integer_part_str}.{decimal_part_str}"
 
-def calculate_binary(number):
-    part_of_integer = integer_part(number)
-    part_of_decimal = decimal_part(number)
-    return f"{part_of_integer}.{part_of_decimal}"
+        # if decimal_part_str:
+        #     return f"{integer_part_str}.{decimal_part_str}"
+        # else:
+        #     return integer_part_str
 
-def integer_part(number):
-    if number == 0:
-        return "0"
+    def integer_part(self, number):
+        result = ""
+        if number == 0:
+            result += str(number)
+            return result
+        else:
+            while number > 0:
+                remainder = number % 2
+                result += str(remainder)
+                number = number // 2
+            return result[::-1]
 
-    result = ""
-    number_of_integer = int(number)
+    def decimal_part(self, number):
+        result = ""
+        while number != 0:
+            number = float(number) * 2
+            if number >= 1:
+                result += '1'
+                number -= 1
+            else:
+                result += '0'
+            if len(result) >= 10:
+                break
+        return result
 
-    while number_of_integer > 0:
-        remainder = number_of_integer % 2
-        result = str(remainder) + result
-        number_of_integer = number_of_integer // 2
-
-    return result
-
-
-def decimal_part(number):
-   decimal_part = math.modf(number)
-   if decimal_part == 0:
-       return ''
-
-   binary = ''
-   while decimal_part > 0:
-       decimal_part *= 2
-       bit = int(decimal_part)
-       binary += str(bit)
-       decimal_part -= bit
-   return binary[:10]
-
-
+    def __str__(self):
+        return self.get_number()
 
 @app.route('/', methods=['GET'])
 def binary_representation_api():
-   number = request.args.get('number')
+    # Get the 'number' parameter from the query string
+    number = request.args.get('number')
+    # Check if 'number' parameter is missing
+    if number is None:
+        return jsonify({"error": "Please send a GET request to /?number=<number>"}), 400
 
-   if number is None:
-       return jsonify({"error": "Please send a GET request to /?number=<number>"}), 400
+    try:
+        # Convert the input string to a boolean or a number
+        if number.lower() == 'true':
+            number = True
+        elif number.lower() == 'false':
+            number = False
+        else:
+            number = float(number)
+    except ValueError:
+        return jsonify({"error": "Please send a GET request to /?number=<number> with a valid number"}), 400
+    except TypeError:
+        return jsonify({"error": "Invalid input type, expected a valid number"}), 400
 
-   try:
-       # Convert the string to a boolean or a number
-       if number.lower() == 'true':
-           number = True
-       elif number.lower() == 'false':
-           number = False
-       else:
-           number = float(number)
-   except ValueError:
-       return jsonify({"error": "Please send a GET request to /?number=<number> with a valid number"}), 400
-
-   binary_representation = BinaryRepresentation(number)
-   return jsonify({"binary_representation": f"{binary_representation.get_number()}"}), 200
-
+    # Create an instance of the BinaryRepresentation class
+    binary_representation = BinaryRepresentation(number)
+    
+    # Return the binary representation as a string
+    binary_string = binary_representation.get_number()
+    return jsonify({"binary_representation": binary_string}), 200
 
 @app.errorhandler(400)
 def bad_request(e):
