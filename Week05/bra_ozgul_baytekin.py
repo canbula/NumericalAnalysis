896fd1306e4ce177b604df4d5c45c6e5109ee89f
Özgül BAYTEKİN <125576234+ozgulbaytekin@users.noreply.github.com>
2023-11-13 20:38:14 +0300

    Update bra_ozgul_baytekin.py

--- Week05/bra_ozgul_baytekin.py
+++ Week05/bra_ozgul_baytekin.py
@@ -1,74 +1,41 @@
-import pytest
 from flask import Flask, request, jsonify
-import os
+import numpy 
 
-class BinaryRepresentation:
-    def __init__(self, number):
-        self.number = number
+app = Flask(__name__)
 
-    def to_binary(self, decimal_places=10):
-        try:
-            number = float(self.number)
-            integer_part = int(number)
-            binary_integer = bin(integer_part)[2:]
-            fractional_part = number - integer_part
+def to_binary(number, decimal_places=10):
+    try:
+        number = float(number)
+        integer_part = int(number)
+        binary_integer = bin(integer_part)[2:]
+        fractional_part = number - integer_part
 
-            binary_fraction = ""
-            for _ in range(decimal_places):
-                fractional_part *= 2
-                bit = int(fractional_part)
-                binary_fraction += str(bit)
-                fractional_part -= bit
+        binary_fraction = ""
+        for _ in range(decimal_places):
+            fractional_part *= 2
+            bit = int(fractional_part)
+            binary_fraction += str(bit)
+            fractional_part -= bit
 
-            binary_representation = binary_integer
-            if binary_fraction:
-                binary_representation += "." + binary_fraction
+        binary_representation = binary_integer
+        if binary_fraction:
+            binary_representation += "." + binary_fraction
 
-            return binary_representation
-        except ValueError:
-            return None
+        return binary_representation
+    except ValueError:
+        return None
 
-files = [f for f in os.listdir(os.path.dirname(__file__)) if f.startswith("bra")]
-for f in files:
-    exec("import " + f[:-3] + " as " + f[:-3])
+@app.route('/binary', methods=['GET'])
+def convert_to_binary():
+    number = request.args.get('number', type=float)
+    if number is None:
+        return jsonify({"error": "Invalid number"}), 400
 
-@pytest.fixture
-def clients():
-    return [eval(f[:-3]).app.test_client() for f in files]
+    binary_repr = to_binary(number)
+    if binary_repr is None:
+        return jsonify({"error": "Invalid number type"}), 400
 
-def test_binary_representation(clients):
-    for client in clients:
-        response = client.get("/binary?number=1")
-        assert response.status_code == 200
-        assert response.json == {"binary_representation": "1."}
+    return jsonify({"binary_representation": binary_repr})
 
-        response = client.get("/binary?number=0.5")
-        assert response.status_code == 200
-        assert response.json == {"binary_representation": "0.1"}
-
-
-def test_binary_representation_error(clients):
-    for client in clients:
-        response = client.get("/binary")
-        assert response.status_code == 400
-        assert response.json == {"error": "Invalid number"}
-
-        response = client.get("/binary?number=abc")
-        assert response.status_code == 400
-        assert response.json == {"error": "Invalid number type"}
-
-
-
-def test_binary_representation_api():
-    for f in files:
-        number = 13.375
-        binary_repr = eval(f[:-3]).BinaryRepresentation(number)
-        assert binary_repr.to_binary() == "1101.011"
-
-
-def test_binary_representation_api_error():
-    for f in files:
-        with pytest.raises(TypeError):
-            eval(f[:-3]).BinaryRepresentation("abc")
-
-    
+if __name__ == '__main__':
+    app.run(debug=True)
