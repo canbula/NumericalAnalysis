abb9780a9ff5e15192c6155f69b1327bc4f87241
Bora Canbula <bora.canbula@cbu.edu.tr>
2023-11-14 17:34:11 +0300

    Codes 4 W07

--- Week07/IEEE754.py
+++ Week07/IEEE754.py
@@ -6,30 +6,29 @@ class IEEE754:
         self,
         number: str = "0.0",
         precision: int = 2,
-        force_length: int = None,
         force_exponent: int = None,
         force_mantissa: int = None,
     ) -> None:
         getcontext().prec = 256
         self.precision: int = precision
-        length_list: list = [16, 32, 64, 128, 256]
-        exponent_list: list = [5, 8, 11, 15, 19]
-        mantissa_list: list = [10, 23, 52, 112, 236]
-        self.__length: int = (
-            force_length if force_length is not None else length_list[precision]
-        )
+        exponent_list: list[int] = [5, 8, 11, 15, 19]
+        mantissa_list: list[int] = [10, 23, 52, 112, 236]
         self.__exponent: int = (
-            force_exponent if force_exponent is not None else exponent_list[precision]
+            force_exponent
+            if force_exponent is not None
+            else exponent_list[self.precision]
         )
         self.__mantissa: int = (
-            force_mantissa if force_mantissa is not None else mantissa_list[precision]
+            force_mantissa
+            if force_mantissa is not None
+            else mantissa_list[self.precision]
         )
         self.__bias: int = 2 ** (self.__exponent - 1) - 1
         self.__edge_case: str = None
         self.number: Decimal = self.validate_number(number)
         if self.__edge_case is None:
-            self.__sign: str = self.find_sign(self.number)
-            self.__scale, self.number = self.scale_up_to_integer(self.number)
+            self.sign: str = self.find_sign()
+            self.__scale, self.number = self.scale_up_to_integer(self.number, 2)
             self.binary: str = f"{self.number:b}"
             self.binary_output: str = (
                 f"{self.binary[:-self.__scale]}.{self.binary[-self.__scale:]}"
@@ -42,27 +41,34 @@ class IEEE754:
             number = "0.0"
         if Decimal(number).is_infinite():
             if Decimal(number) > 0:
+                # +inf: 0 11111111 00000000000000000000000
                 self.__edge_case = f"0 {'1' * self.__exponent} {'0' * self.__mantissa}"
                 return Decimal("Infinity")
+            # -inf: 1 11111111 00000000000000000000000
             self.__edge_case = f"1 {'1' * self.__exponent} {'0' * self.__mantissa}"
             return Decimal("-Infinity")
         if Decimal(number).is_nan() and Decimal(number).is_snan():
+            # snan: 0 11111111 00000000000000000000001
             self.__edge_case = (
                 f"0 {'1' * self.__exponent} {'0' * (self.__mantissa - 1)}1"
             )
             return Decimal("NaN")
         if Decimal(number).is_nan() and Decimal(number).is_qnan():
+            # qnan: 0 11111111 10000000000000000000000
             self.__edge_case = (
                 f"0 {'1' * self.__exponent} 1{'0' * (self.__mantissa - 1)}"
             )
             return Decimal("NaN")
         if not number == number:
+            # nan: 0 11111111 11111111111111111111111
             self.__edge_case = f"0 {'1' * self.__exponent} {'1' * self.__mantissa}"
             return Decimal("NaN")
         if Decimal(number) == 0:
             if Decimal(number).is_signed():
+                # -0: 1 00000000 00000000000000000000000
                 self.__edge_case = f"1 {'0' * self.__exponent} {'0' * self.__mantissa}"
             else:
+                # +0: 0 00000000 00000000000000000000000
                 self.__edge_case = f"0 {'0' * self.__exponent} {'0' * self.__mantissa}"
             return Decimal("0")
         if isinstance(number, int):
@@ -95,41 +101,83 @@ class IEEE754:
 
         return (smallest_denormalized, largest_denormalized)
 
-    @staticmethod
-    def scale_up_to_integer(number: Decimal) -> (int, int):
-        scale = 0
-        while number != int(number):
-            number *= 2
-            scale += 1
-        return scale, int(number)
-
-    @staticmethod
-    def find_sign(number: int) -> str:
-        if number < 0:
+    def find_sign(self) -> str:
+        if self.number < 0:
             return "1"
         return "0"
 
+    @staticmethod
+    def scale_up_to_integer(number: Decimal, base: int) -> (int, int):
+        scale = 0
+        while number != int(number):
+            number *= base
+            scale += 1
+        return scale, int(number)
+
     def find_exponent(self) -> str:
         exponent = len(self.binary) - 1 + self.__bias - self.__scale
+        # fill with leading zeros if necessary
         return f"{exponent:0{self.__exponent}b}"
 
     def find_mantissa(self) -> str:
+        # fill with trailing zeros if necessary
         mantissa = f"{self.binary[1:]:<0{self.__mantissa}}"
         if len(mantissa) > self.__mantissa:
-            mantissa = f"{mantissa[: self.__mantissa - 1]}1"  # round up
+            # round up if mantissa is too long
+            mantissa = f"{mantissa[: self.__mantissa - 1]}1"
         return mantissa
 
     def __str__(self) -> str:
         if self.__edge_case is not None:
             return self.__edge_case
-        return f"{self.__sign} {self.exponent} {self.mantissa}"
+        return f"{self.sign} {self.exponent} {self.mantissa}"
+
+    def hex(self) -> str:
+        h = ""
+        s = str(self).replace(" ", "")
+        limit = len(s) - (len(s) % 4)
+        for i in range(0, limit, 4):
+            ss = s[i : i + 4]
+            si = 0
+            for j in range(4):
+                si += int(ss[j]) * (2 ** (3 - j))
+            sh = hex(si)
+            h += sh[2]
+        return h.upper()
+
+    def json(self) -> dict:
+        return {
+            "exponent-bits": self.__exponent,
+            "mantissa-bits": self.__mantissa,
+            "bias": self.__bias,
+            "sign": self.sign,
+            "exponent": self.exponent,
+            "mantissa": self.mantissa,
+            "binary": self.binary,
+            "binary_output": self.binary_output,
+            "hex": self.hex(),
+            "up scaled number": self.number,
+            "scale": self.__scale,
+            "number": self.number / (2**self.__scale),
+        }
 
 
 if __name__ == "__main__":
-    number = "0.000000000000000000000004"
-    number = "4e-24"
-    number = 1e-38
-    number = -0
-    print(f"number: {number}")
-    ieee754_number = IEEE754(number=number, precision=2)
-    print(f"ieee754: {ieee754_number}")
+    # with default options (Double Precision)
+    x = 13.375
+    a = IEEE754(x)
+    # you should call the instance as a string
+    print(a)
+    print(str(a))
+    print(f"{a}")
+    # you can get the hexadecimal presentation like this
+    print(a.hex())
+    # or you can specify a precision and
+    for p in range(5):
+        a = IEEE754(x, p)
+        print("x = %f | b = %s | h = %s" % (13.375, a, a.hex()))
+    # or you can use your own custom precision
+    a = IEEE754(x, force_exponent=6, force_mantissa=12)
+    print(f"{a}")
+    # you can get more details with json
+    print(a.json())
