c1383bf7dd6e91ae039a0a1d3188067a2e6c9c9a
YASER BEKER <79779674+yasserbakkar97@users.noreply.github.com>
2023-11-19 05:35:28 +0300

    Update bra_yaser_beker.py

--- Week05/bra_yaser_beker.py
+++ Week05/bra_yaser_beker.py
@@ -3,43 +3,54 @@ from flask import Flask, request, jsonify
 class BinaryRepresentation:
     def __init__(self, number):
         self.number = number
-        self.integer_part = int(number)
-        self.decimal_part = number - self.integer_part
 
     def integer2binary(self):
-        return bin(self.integer_part)[2:]
-    
+        integer_part = int(self.number)
+        binary_string = ""
+        while integer_part > 0:
+            remainder = integer_part % 2
+            integer_part //= 2
+            binary_string += str(remainder)
+
+        return binary_string[::-1]
+
     def decimal2binary(self):
-        decimal_binary = ""
-        decimal_part = self.decimal_part
+        decimal_part = abs(self.number - int(self.number))
+        binary_string = ""
 
-        for _ in range(10):
+        while decimal_part != 0 and len(binary_string) < 10:
             decimal_part *= 2
-            bit = int(decimal_part)
-            decimal_binary += str(bit)
-            decimal_part -= bit
+            binary_integer_part = int(decimal_part)
+            binary_string += str(binary_integer_part)
+            decimal_part -= binary_integer_part
 
-        return decimal_binary.ljust(10, '0')
-    
+        return binary_string
+
     def __str__(self):
         integer_binary = self.integer2binary()
         decimal_binary = self.decimal2binary()
-        return {'binary_representation': f"{integer_binary}.{decimal_binary}"}
-    
+
+         # Ensure integer part is always displayed
+        if integer_binary == "":
+            integer_binary = "0"
+            
+        return f"{integer_binary}.{decimal_binary}"
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def get_binary_representaion():
+@app.route('/', methods=['GET'])
+def convert_to_binary():
     try:
-        number = float(request.args.get("number"))
+        number = float(request.args['number'])
+        binary_representation = BinaryRepresentation(number)
+        return jsonify({'binary_representation': str(binary_representation)})
     except ValueError:
-        return jsonify({'error': 'Please send a GET request to /?number=<number> with a valid number'}), 400
-    except:
-        return jsonify({"error": "Please send a GET request to /?number=<number>"}), 400
+        return jsonify({'error': 'Please send a GET request to /?number=<number> with a valid number'})
+    except KeyError:
+        return jsonify({'error': 'Please send a GET request to /?number=<number>'})
+    except TypeError:
+        return jsonify({'error': 'Please send a GET request to /?number=<number> with a valid number'})
 
-    
-    binary_representation = BinaryRepresentation(number)
-    return jsonify({"result": str(binary_representation)})
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
