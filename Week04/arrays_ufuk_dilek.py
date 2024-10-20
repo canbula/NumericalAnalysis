7cd6f1d201614560ce557acb55efb038dd8fa0a2
Bora Canbula <bora.canbula@cbu.edu.tr>
2024-10-20 14:10:18 +0300

    Update arrays_ufuk_dilek.py
    Fixes the issue #101

--- Week04/arrays_ufuk_dilek.py
+++ Week04/arrays_ufuk_dilek.py
@@ -5,7 +5,7 @@ def replace_center_with_minus_one(d, n, m):
     error = m > n or d <= 0 or n < 0 or m < 0
     if error:
         raise ValueError("Problem with the parameters")
-    arr = np.random.randint(0, 10**d, size=(n, n))
+    arr = np.random.randint(10**(d-1), (10**d)-1, size=(n, n))
     start = int((n - m) / 2)
     end = start + m
     arr[start:end, start:end] = -1
