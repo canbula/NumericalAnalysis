a54778b5040dabd160dead572f01a11b81b5f59b
protagonist9 <bozgan.tarik@gmail.com>
2025-11-14 20:44:27 +0300

    Created arrays_tarik_bozgan.py

--- Week04/arrays_tarik_bozgan.py
+++ Week04/arrays_tarik_bozgan.py
@@ -10,11 +10,12 @@ def replace_center_with_minus_one(d, n, m):
     if m < 0:
         raise ValueError("m must be greater than or equal to 0")
     
-    array = np.random.randint(10**d, size=n)
+    arr = np.random.randint(10**d, size=n)
     
     start_index = (n - m) // 2
     end_index = start_index + m
     
-    array[start_index:end_index] = -1
+    for i in range(start_index, end_index):
+        arr[i] = -1
     
-    return array
+    return arr
